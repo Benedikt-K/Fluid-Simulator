ecccd78d26fa2c0446690a3cde478ad19d3a3fd7
Benedikt Kuss <benedikt.kuss@hotmail.com>
2024-09-08 16:44:26 +0200

    added classes and todo document

---
+++ SPH_Bachelorprojekt/Simulation/MainSimulation/Standard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPH_Bachelorprojekt.Simulation.MainSimulation
+{
+    internal class Standard
+    {
+    }
+}
