c8c828f1c8f76d1f88b06f5f647431d423e317d0
Benedikt Kuss <benedikt.kuss@hotmail.com>
2024-10-21 22:17:36 +0200

    tryed some things

--- SPH_Bachelorprojekt/MainWindow2.cs
+++ SPH_Bachelorprojekt/MainWindow2.cs
@@ -78,8 +78,8 @@ namespace SPH_Bachelorprojekt
                 //List<Particle> particles = spawner.FluidColumWithOutRand();
                 //List<Particle> particles = spawner.DroppingFluidColumn();
                 //List<Particle> particles = spawner.DroppingFluidColumnBig();
-                List<Particle> particles = spawner.BreakingDam();
-                //List<Particle> particles = spawner.BreakingDamOneLayer();
+                //List<Particle> particles = spawner.BreakingDam();
+                List<Particle> particles = spawner.BreakingDamOneLayer();
                 //List<Particle> particles = spawner.BreakingDamBig();
                 //List<Particle> particles = spawner.BreakingDamBigAndWide();
                 //List<Particle> particles = spawner.BreakingDamBigAndWideTestLimit();
