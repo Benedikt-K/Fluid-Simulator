38d787c6e6b7c125edad5a6988bd97e3c9a69bb0
Benedikt Kuss <benedikt.kuss@hotmail.com>
2024-10-25 00:58:16 +0200

    updated location of functions in file, not complete yet

--- SPH_Bachelorprojekt/Simulation/MainSimulation/SESPH.cs
+++ SPH_Bachelorprojekt/Simulation/MainSimulation/SESPH.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+using SPH_Bachelorprojekt.Simulation.Particles;
+using SPH_Bachelorprojekt.Simulation.Kernel_Function;
 
 namespace SPH_Bachelorprojekt.Simulation.MainSimulation
 {
@@ -11,6 +14,25 @@ namespace SPH_Bachelorprojekt.Simulation.MainSimulation
     /// </summary>
     class SESPH
     {
-
+        public Vector2 CalculatePressureAcceleration(Particle particle, List<Particle> neighbours, Kernel kernel)
+        {
+            // boundary handling with mirroring
+            Vector2 pressureAcceleration = Vector2.Zero;
+            Vector2 pressureAccelerationBoundary = Vector2.Zero;
+            float pressureOverDensity2 = particle.Pressure / (particle.Density * particle.Density);
+            foreach (Particle neighbour in neighbours)
+            {
+                Vector2 GradW = kernel.GradW(particle.Position, neighbour.Position);
+                if (neighbour.IsBoundaryParticle)
+                {
+                    pressureAccelerationBoundary += particle.Mass * (pressureOverDensity2 + pressureOverDensity2) * GradW;
+                    continue;
+                }
+                float pressureOverDenisty2Neighbour = neighbour.Pressure / (neighbour.Density * neighbour.Density);
+                pressureAcceleration += neighbour.Mass * (pressureOverDensity2 + pressureOverDenisty2Neighbour) * GradW;
+            }
+            Vector2 totalAcceleration = Vector2.Zero - pressureAcceleration - pressureAccelerationBoundary;
+            return totalAcceleration;
+        }
     }
 }
--- SPH_Bachelorprojekt/Simulation/MainSimulation/SPH.cs
+++ SPH_Bachelorprojekt/Simulation/MainSimulation/SPH.cs
@@ -27,5 +27,44 @@ namespace SPH_Bachelorprojekt.Simulation.MainSimulation
             }
             return density;
         }
+
+        public Vector2 CalculateViscosityAcceleration(Particle particle, List<Particle> neighbours, float ParticleSizeH, float viscosity, Kernel kernel)
+        {
+            Vector2 viscostiyAcc = Vector2.Zero;
+            foreach (Particle neighbour in particle.Neighbours)
+            {
+                Vector2 v_ij = particle.Velocity - neighbour.Velocity;
+                Vector2 x_ij = particle.Position - neighbour.Position;
+
+                Vector2 gradW = kernel.GradW(particle.Position, neighbour.Position);
+                float massDensityRatio = neighbour.Mass / neighbour.Density;
+                float scaledParticleSize = 0.01f * ParticleSizeH * ParticleSizeH;
+                float v_dot_x = Vector2.Dot(v_ij, x_ij);
+                float x_dot_x = Vector2.Dot(x_ij, x_ij);
+                viscostiyAcc += massDensityRatio * (v_dot_x / (x_dot_x + scaledParticleSize)) * gradW;
+            }
+            viscostiyAcc = 2 * viscosity * viscostiyAcc;
+
+            return viscostiyAcc;
+        }
+
+        public Vector2 CalculateSurfaceTensionAcceleration(Particle particle, List<Particle> neighbours, Kernel kernel)
+        {
+            // Have the feeling that it doesnt work properly
+            Vector2 surfaceTensionAcc = Vector2.Zero;
+            foreach (Particle neighbor in particle.Neighbours)
+            {
+                Vector2 gradW = kernel.GradW(particle.Position, neighbor.Position);
+                float factor = 0f;         // works with 10, but gets not completely round
+                surfaceTensionAcc += gradW * factor;
+            }
+            return surfaceTensionAcc;
+        }
+
+        public float CalculateParticleLambdaCFL(Vector2 velocity, float ParticleSizeH, float TimeStep)
+        {
+            float lambda = (TimeStep * velocity.Length()) / ParticleSizeH;
+            return lambda;
+        }
     }
 }
