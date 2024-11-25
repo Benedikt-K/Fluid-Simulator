aa02eb5b5938392631dec4093cb3231ee75e384f
Benedikt Kuss <benedikt.kuss@hotmail.com>
2024-11-25 16:29:05 +0100

    better variable names

--- SPH_Bachelorprojekt/Simulation/Neighbours/SpatialHashing.cs
+++ SPH_Bachelorprojekt/Simulation/Neighbours/SpatialHashing.cs
@@ -8,16 +8,21 @@ namespace SPH_Bachelorprojekt.Simulation.Neighbours
 {
     public class SpatialHashing
     {
+        //
+        // partially based on https://stackoverflow.com/questions/39580691/how-to-find-neighbor-squares-in-spatial-hashing
+        // and https://cg.informatik.uni-freiburg.de/course_notes/sim_03_particleFluids.pdf
+        //
+
         public int Count;
         public int CellSize;
-        private readonly Dictionary<Vector2, HashSet<Particle>> SpatialGrid = new Dictionary<Vector2, HashSet<Particle>>();
+        public Dictionary<Vector2, HashSet<Particle>> DictionarySpatial = new Dictionary<Vector2, HashSet<Particle>>();
 
         public SpatialHashing(int cellSize)
         {
             CellSize = cellSize;
         }
 
-        public Vector2 Hash(Vector2 vector)
+        public Vector2 HashFunction(Vector2 vector)
         {
             vector = Vector2.Divide(vector, CellSize);
             Vector2 vectorFloored = new Vector2((float) Math.Floor(vector.X), (float) Math.Floor(vector.Y));
@@ -26,68 +31,61 @@ namespace SPH_Bachelorprojekt.Simulation.Neighbours
 
         public void AddParticle(Particle particle)
         {
-            Vector2 hash = Hash(particle.Position);
-            if (!SpatialGrid.TryGetValue(hash, out var objectBucket))
+            Vector2 hash = HashFunction(particle.Position);
+            if (!DictionarySpatial.TryGetValue(hash, out HashSet<Particle> newHashSet))
             {
-                objectBucket = new HashSet<Particle>();
-                SpatialGrid[hash] = objectBucket;
+                newHashSet = new HashSet<Particle>();
+                DictionarySpatial[hash] = newHashSet;
             }
-            objectBucket.Add(particle);
-            Count++;
+            newHashSet.Add(particle);
+            Count = Count + 1;
         }
 
         public void RemoveParticle(Particle particle)
         {
-            var hash = Hash(particle.Position);
-            if (!SpatialGrid.TryGetValue(hash, out HashSet<Particle> objectBucket))
+            Vector2 hash = HashFunction(particle.Position);
+            if (!DictionarySpatial.TryGetValue(hash, out HashSet<Particle> newHashSet))
             {
                 return;
             }
-            if (!objectBucket.Remove(particle))
+            if (!newHashSet.Remove(particle))
             {
                 return;
             }
-            Count--;
-            SpatialGrid[hash] = objectBucket;
-            if (objectBucket.Count == 0)
+            Count = Count - 1;
+            DictionarySpatial[hash] = newHashSet;
+            if (newHashSet.Count == 0)
             {
-                SpatialGrid.Remove(hash);
+                DictionarySpatial.Remove(hash);
             }
         }
 
-        public void Clear()
+        public void GetNeighbours(Vector2 position, float searchRadius, ref List<Particle> foundNeighbours)
         {
-            SpatialGrid.Clear();
-        }
+            foundNeighbours.Clear();
+            int startPos_X = (int) Math.Floor((position.X - searchRadius) / CellSize);
+            int endPos_X = (int) Math.Ceiling((position.X + searchRadius) / CellSize);
+            int startPos_Y = (int) Math.Floor((position.Y - searchRadius) / CellSize);
+            int endPos_Y = (int) Math.Ceiling((position.Y + searchRadius) / CellSize);
+            IEnumerable<int> x_positions = Enumerable.Range(startPos_X, endPos_X - startPos_X + 1);
+            IEnumerable<int> y_positions = Enumerable.Range(startPos_Y, endPos_Y - startPos_Y + 1);
 
-        public void GetNeighbours(Vector2 position, float radius, ref List<Particle> particleInSearchRadius)
-        {
-            particleInSearchRadius.Clear();
-            int startPos_X = (int) Math.Floor((position.X - radius) / CellSize);
-            int endPos_X = (int) Math.Ceiling((position.X + radius) / CellSize);
-            int startPos_Y = (int) Math.Floor((position.Y - radius) / CellSize);
-            int endPos_Y = (int) Math.Ceiling((position.Y + radius) / CellSize);
-            IEnumerable<int> xRange = Enumerable.Range(startPos_X, endPos_X - startPos_X + 1);
-            IEnumerable<int> yRange = Enumerable.Range(startPos_Y, endPos_Y - startPos_Y + 1);
-
-            foreach (int x in xRange)
+            foreach (int x in x_positions)
             {
-                foreach (int y in yRange)
+                foreach (int y in y_positions)
                 {
-                    var hash = new Vector2(x, y);
-                    if (!SpatialGrid.ContainsKey(hash))
+                    Vector2 hashValue = new Vector2(x, y);
+                    if (DictionarySpatial.ContainsKey(hashValue))
                     {
-                        continue;
-                    }
-                    HashSet<Particle> objectsInBucket = SpatialGrid[hash];
-                    foreach (Particle particle in objectsInBucket)
-                    {
-                        float distance = Vector2.Distance(particle.Position, position);
-                        if (distance > radius)
+                        HashSet<Particle> currentCellHasSet = DictionarySpatial[hashValue];
+                        foreach (Particle particle in currentCellHasSet)
                         {
-                            continue;
+                            float distance = Vector2.Distance(particle.Position, position);
+                            if (distance <= searchRadius)
+                            {
+                                foundNeighbours.Add(particle);
+                            }
                         }
-                        particleInSearchRadius.Add(particle);
                     }
                 }
             }
